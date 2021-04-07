151bac19a6e45e6bee104f3c55c4c26158f82114
DaviRibeiro-prog <76858682+DaviRibeiro-prog@users.noreply.github.com>
2021-04-07 16:29:15 -0300

    Add files via upload

---
+++ JavaSwing/JavaSwing + Sql/src/SistemaCadastro/ConnectionSql.java
@@ -0,0 +1,16 @@
+package loja;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionSql {
+	
+	public Connection connectionMySql() throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/loja_java?useTimezone=true&serverTimezone=UTC", "root", "");
+		return conn;
+	}
+	
+	
+	
+}
